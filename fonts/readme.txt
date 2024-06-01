d509afada9c8f2b5e140c6256ed9de5d52f973d4
lizaremizova <er115@inbox.lv>
2024-06-01 11:53:56 +0300

    first commit

---
+++ fonts/readme.txt
@@ -0,0 +1,17 @@
+Thanks for your attention! 
+
+Free font here DOWNLOAD Beta Version!
+
+If you use this font in your projects,
+please tag and add a link to my Behance.
+And of course, make sure to like and follow.
+https://www.behance.net/daniyarz
+
+Follow me Instagram 
+https://www.instagram.com/shape.plus/ 
+
+Telegram
+https://t.me/daniyarabout
+
+Want a design just as cool?
+Email me: daniyarsharipov@yandex.com
---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, url_for, redirect
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+app = Flask(__name__)
+
+app.config.update(
+    SECRET_KEY = 'WOW SUCH SECRET'
+)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+@login_manager.user_loader
+def load_user(login):
+    if login == 'admin':
+        return User(login)
+
+@app.route('/')
+@login_required
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    login = 'admin'
+    password = 'admin'
+    if request.method == 'POST':
+        if request.form['login'] == login and request.form['password'] == password:
+            user = User(login) # Создаем пользователя
+            login_user(user) # Логинем пользователя
+            return redirect(url_for('index'))
+        else:
+            return render_template('login.html', error='Неправильный логин или пароль')
+    return render_template('login.html')
+
+app.run(debug=True)
